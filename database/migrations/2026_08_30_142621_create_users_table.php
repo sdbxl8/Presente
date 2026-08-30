4d490f833acfe5c00a391f9ba9a5e5c34570995e
sdbxl8 <99salva@gmail.com>
2026-08-30 16:34:11 +0200

    Add initial model and migration files for Attendance, ClassSession, Group, Justification, Subject, and users

---
+++ database/migrations/2026_08_30_142621_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('role');
+            $table->string('photo')->nullable();
+            $table->string('degree')->nullable();
+            $table->string('course')->nullable();
+
+            $table->foreignId('group_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
